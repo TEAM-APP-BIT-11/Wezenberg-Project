752886a8ed8243146f6870b456274d57828a669f
Senglide <33479261+Senglide@users.noreply.github.com>
2018-02-25 15:06:49 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => getenv('DB_HOSTNAME'),
-	'username' => getenv('DB_USERNAME'),
-	'password' => getenv('DB_PASSWORD'),
-	'database' => getenv('DB_DATABASE'),
+	'hostname' => getenv('DB_HOSTNAME');,
+	'username' => getenv('DB_USERNAME');,
+	'password' => getenv('DB_PASSWORD');,
+	'database' => getenv('DB_DATABASE');,
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
