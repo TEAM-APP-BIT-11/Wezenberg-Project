828e4b449706d4c5bf4b76755a37aa40bd453014
Neil VdB <neil2223@hotmail.com>
2018-03-02 15:35:35 +0100

    typfout wedstrijdresultaten beheren aangepast

--- application/views/trainer/wedstrijdresultaten_beheren.php
+++ application/views/trainer/wedstrijdresultaten_beheren.php
@@ -1,6 +1,6 @@
 <h1 class="">Wedstrijd resultaten beheren</h1>
 <div class="row">
-    <div class="col-md-8 coll-md-offset-2">
+    <div class="col-md-8 col-md-offset-2">
         <table class="table">
             <tr class="active">
                 <td>Naam</td>
