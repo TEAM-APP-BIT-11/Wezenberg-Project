b2072cca94012c9eb923abe692ecfab1dc17e5c7
Senglide <sennecools@hotmail.com>
2018-03-02 15:21:29 +0100

    Fixing config.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => getenv(''),
+	'hostname' => getenv('DB_HOSTNAME'),
 	'username' => getenv('DB_USERNAME'),
 	'password' => getenv('DB_PASSWORD'),
 	'database' => getenv('DB_DATABASE'),
