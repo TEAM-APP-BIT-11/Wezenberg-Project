29ed2993d51dd413818e2b1e846b6bb91d723863
dieterverboven <dieter.verboven@outlook.com>
2018-03-02 15:37:15 +0100

    Code voor locatie_beheren en wedstrijd_aanvragen

--- application/views/trainer/locatie_beheren.php
+++ application/views/trainer/locatie_beheren.php
@@ -6,3 +6,16 @@
  * and open the template in the editor.
  */
 
+?>
+
+<h1>Locaties Beheren</h1>
+<h3>Lijst van alle locaties</h3>
+
+<table class="table">
+    <thead>
+        <th>Locatie</th>
+        <th>Acties</th>
+    <thead>
+</table>
+
+<button>Voeg een nieuwe locatie toe</button>
--- application/views/zwemmer/wedstrijd_aanvragen.php
+++ application/views/zwemmer/wedstrijd_aanvragen.php
@@ -6,3 +6,20 @@
  * and open the template in the editor.
  */
 
+?>
+
+
+<h1>Wedstrijd Aanvragen</h1>
+<h3>Lijst van alle wedstrijden</h3>
+
+<table class="table">
+    <thead>
+        <th>Slag</th>
+        <th>Afstand</th>
+        <th>Datum</th>
+        <th>Uur</th>
+        <th>Aanvragen</th>
+        <th>Status</th>
+    <thead>
+</table>
+        
