de10a9bbeb1b6759f465e17aa302c5b5beee217a
stefjs <r0655338@student.thomasmore.be>
2018-03-02 15:10:00 +0100

    home, wedstrijd_beheren en wedstrijd_aanpassen

--- application/views/trainer/wedstrijden_beheren.php
+++ application/views/trainer/wedstrijden_beheren.php
@@ -1,23 +1,71 @@
-<h1 class="">Wedstrijd resultaten beheren</h1>
-<div class="row">
-    <div class="col-md-8 coll-md-offset-2">
-        <table class="table">
-            <tr class="active">
-                <td>Naam</td>
-                <td>Acties</td>
-            </tr>
-            <tr>
-                <td>XXXX</td>
-                <td><a href="#">Aanpassen / Toevoegen</a></td>
-            </tr>
-            <tr>
-                <td>XXXX</td>
-                <td><a href="#">Aanpassen / Toevoegen</a></td>
-            </tr>
-            <tr>
-                <td>XXXX</td>
-                <td><a href="#">Aanpassen / Toevoegen</a></td>
-            </tr>
-        </table>
-    </div>
+<nav class="navbar navbar-default navbar-static-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				Wezenberg
+			</a>
+		</div>
+
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="dropdown ">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+						Trainer
+						<span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Logout</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="container-fluid main-container">
+	<div class="col-md-2 sidebar">
+		<ul class="nav nav-pills nav-stacked">
+			<li><a href="#">Home</a></li>
+			<li class="active"><a href="#">Wedstrijd beheren</a></li>
+			<li><a href="#">Wedstrijd aanvragen</a></li>
+			<li><a href="#">Locatie beheren</a></li>
+			<li><a href="#">Resultaten bekijken</a></li>
+			<li><a href="#">Resultaten beheren</a></li>
+			<li><a href="#">Gebruikers</a></li>
+			<li><a href="#">Schema supplementen</a></li>
+			<li><a href="#">Supplementen beheren</a></li>
+			<li><a href="#">Homepagina beheren</a></li>
+		</ul>
+	</div>
+	<div class="col-md-10 content">
+
+		<h1 class="">Wedstrijden beheren</h1>
+		<hr>
+		<h3>Wedstrijden</h3>
+
+		<table class="table">
+			<tr>
+				<th>Naam</th>
+				<th>Locatie</th>
+				<th>Datum</th>
+				<th>Actie</th>
+			</tr>
+			<tr>
+				<td>Wedstrijd x</td>
+				<td>Geel</td>
+				<td>01/01/2001</td>
+				<td><a href="">Details</a></td>
+			</tr>
+		</table>
+
+		<button type="button" class="btn btn-primary">Wedstrijd toevoegen</button>
+	</div>
+
+	<footer>
+	</footer>
+
 </div>
