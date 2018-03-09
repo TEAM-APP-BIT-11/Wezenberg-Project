0e24555c7281e041abe74600740f9bdce764a33b
Neil VdB <neil2223@hotmail.com>
2018-03-09 15:29:44 +0100

    Zwemmer toevoeging wedstrijd inschrijven

---
+++ application/views/zwemmer_menu.php
@@ -0,0 +1,5 @@
+    <li class="active"><?php echo anchor('/trainer/Wedstrijd/beheren', 'Home', '')?></li>
+    <li><?php echo anchor('/zwemmer/Wedstrijd/inschrijven', 'inschrijven Wedstrijd', '')?></li>
+    <li><?php echo anchor('/trainer/Locatie/beheren', 'Locatie beheren', '')?></li>
+    <li><?php echo anchor('/trainer/Wedstrijd/beheren', 'Resultaten bekijken', '')?></li>
+    <li><?php echo anchor('/trainer/Wedstrijd/beheren', 'Homepagina beheren', '')?></li>
