5628bdb4c46a6c7be5a2a2f48ca70256f6b0a69b
dieterverboven <dieter.verboven@outlook.com>
2018-03-09 15:43:17 +0100

    Agenda raadplegen gegevens

--- application/models/Evenement_model.php
+++ application/models/Evenement_model.php
@@ -35,6 +35,21 @@ class Evenement_model extends CI_Model {
 		$this->db->insert('evenement_model', $evenement);
 		return $this->db->insert_id();
 	}
+        
+        function getLocatieWithLocatieByDeelnemer($id) {
+        $this->db->where('id', $id);
+        $query = $this->db->get('evenement');
+
+        $product = $query->row();
+
+        $this->load->model('locatie_model');
+        $product->soort = $this->soort_model->get($product->soortId);
+
+        $this->load->model('brouwerij_model');
+        $product->brouwerij = $this->brouwerij_model->get($product->brouwerijId);
+
+        return $product;
+        }
 
 }
 
