329e500f1ea813f8bd6d5732b4392c3508ddd66f
stefjs <r0655338@student.thomasmore.be>
2018-03-23 15:29:29 +0100

    Update

---
+++ application/views/trainer/locatie_toevoegen.php
@@ -0,0 +1,71 @@
+<div class="col-md-10 content">
+
+		<h1 class="">Locaties beheren</h1>
+		<hr>
+		<h3>Locatie toevoegen</h3>
+    <h4 class="text-center">Aanpassen</h4>
+    <form class="form-horizontal">
+<fieldset>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatieNaam">Naam</label>
+  <div class="col-md-4">
+  <input id="locatieNaam" name="locatieNaam" value="" placeholder="placeholder" class="form-control input-md" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatieStraat">Straat</label>
+  <div class="col-md-4">
+  <input id="locatieStraat" name="locatieStraat" value="" placeholder="placeholder" class="form-control input-md" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatieNr">Nummer</label>
+  <div class="col-md-4">
+  <input id="locatieNr" name="locatieNr" value="" placeholder="placeholder" class="form-control input-md" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatiePostcode">Postcode</label>
+  <div class="col-md-4">
+  <input id="locatiePostcode" name="locatiePostcode" value="" placeholder="placeholder" class="form-control input-md" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatieGemeente">Gemeente</label>
+  <div class="col-md-4">
+  <input id="locatieGemeente" name="locatieGemeente" value="" placeholder="placeholder" class="form-control input-md" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatieZaal">Zaal</label>
+  <div class="col-md-4">
+  <input id="locatieZaal" name="locatieZaal" value="" placeholder="placeholder" class="form-control input-md" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatieLand">Land</label>
+  <div class="col-md-4">
+  <input id="locatieLand" name="locatieLand" value="" placeholder="placeholder" class="form-control input-md" type="text">
+  </div>
+</div>
+
+<div class="form-group">
+  <label class="col-md-4 control-label" for="locatieExtraInfo">Extra informatie</label>
+  <div class="col-md-4">
+    <textarea class="form-control" id="locatieExtraInfo" name="textarea"></textarea>
+  </div>
+</div>
+</fieldset>
+
+<a href="javascript:history.go(-1);"><button type="button" class="btn btn-secundary">Annuleren</button></a>
+<button type="button" class="btn btn-primary">Opslaan</button>
+
+</form>
+</div>
