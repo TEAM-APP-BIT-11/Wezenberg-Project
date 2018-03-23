abdcd821ec39c908ac4e49a162f4d8ef21c6d125
Neil VdB <neil2223@hotmail.com>
2018-03-23 14:09:32 +0100

    Inloggen gefixed

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,39 +1,73 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->helper('url');
-                
-                $partials = array(
-                    'inhoud' => 'main_menu');
-                $this->template->load('main_master', $partials);
-	}
-        
-        public function toon() {
-            $data['titel'] = 'Formulier met dialoogvenster';
-            $data['naam'] = 'Neil';
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     *        http://example.com/index.php/welcome
+     *    - or -
+     *        http://example.com/index.php/welcome/index
+     *    - or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
 
-            $partials = array(
-                'menuGebruiker' => 'trainer_menu',
-                'inhoud' => 'trainer/home');
-            $this->template->load('main_master', $partials, $data);
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->helper('form');
+        $this->load->helper('notation');
+    }
+
+    public function index()
+    {
+        $this->load->helper('url');
+
+        $partials = array(
+            'inhoud' => 'algemeen/inloggen');
+        $this->template->load('main_master', $partials);
+    }
+
+    public function controleerAanmelden()
+    {
+        $data['titel'] = 'Formulier met dialoogvenster';
+        $data['naam'] = 'Neil';
+
+        $gebruikersnaam = $this->input->post('gebruikersnaam');
+        $wachtwoord = $this->input->post('wachtwoord');
+
+        if ($this->authex->meldAan($gebruikersnaam, $wachtwoord)) {
+            $gebruiker = $this->autex->getGebruikerInfo();
+            if ($gebruiker->typePersoonId == 1) {
+                redirect('zwemmer/Home');
+            } else {
+                redirect('trainer/Home');
+            }
+
+        } else {
+            //fout 
+            redirect('Welcome/index');
         }
+        echo $this->authex->meldAan($gebruikersnaam, $wachtwoord);
+    }
+
+    public function toon()
+    {
+        $data['titel'] = 'Formulier met dialoogvenster';
+        $data['naam'] = 'Neil';
+
+        $partials = array(
+            'menuGebruiker' => 'trainer_menu',
+            'inhoud' => 'trainer/home');
+        $this->template->load('main_master', $partials, $data);
+    }
 }
--- application/models/Persoon_model.php
+++ application/models/Persoon_model.php
@@ -1,40 +1,67 @@
 <?php
-class Persoon_model extends CI_Model {
-	function __construct()
-	{
-		parent::__construct();
-	}
 
-	function get($id)
-	{
-		$this->db->where('id', $id);
-		$query = $this->db->get('persoon');
-		return $query->row();
-	}
+class Persoon_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
 
-	function getAll()
-	{
-		$query = $this->db->get('persoon');
-		return $query->result();
-	}
+    function get($id)
+    {
+        $this->db->where('id', $id);
+        $query = $this->db->get('persoon');
+        return $query->row();
+    }
 
-	function update($persoon)
-	{
-		$this->db->where('id', $persoon->id);
-		$this->db->update('persoon_model', $persoon);
-	}
+    function getPersoon($gebruikersnaam, $wachtwoord)
+    {
+        echo $gebruikersnaam . " - 0" . $wachtwoord;
+        $this->db->where('gebruikersnaam', $gebruikersnaam);
 
-	function delete($id)
-	{
-		$this->db->where('id', $id);
-		$this->db->delete('persoon_model');
-	}
+        $this->db->where('actief', 1);
+        $query = $this->db->get('persoon');
+        echo "rij : " . $query->num_rows();
 
-	function insert($persoon)
-	{
-		$this->db->insert('persoon_model', $persoon);
-		return $this->db->insert_id();
-	}
+        if ($query->num_rows() == 1) {
+            $gebruiker = $query->row();
+            // controleren of het wachtwoord overeenkomt
+            if (password_verify($wachtwoord, $gebruiker->wachtwoord)) {
+                echo "wachtwoord ok";
+                return $gebruiker;
+            } else {
+                echo "wachtwoord fout";
+                return null;
+            }
+        } else {
+            echo "geen gebruiker";
+            return null;
+        }
+    }
+
+    function getAll()
+    {
+        $query = $this->db->get('persoon');
+        return $query->result();
+    }
+
+    function update($persoon)
+    {
+        $this->db->where('id', $persoon->id);
+        $this->db->update('persoon_model', $persoon);
+    }
+
+    function delete($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('persoon_model');
+    }
+
+    function insert($persoon)
+    {
+        $this->db->insert('persoon_model', $persoon);
+        return $this->db->insert_id();
+    }
 
 }
 
