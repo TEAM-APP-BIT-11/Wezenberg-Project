7a827b5fd90b1d0d627c66d09d2bee0ad114fabb
Senne <sennecools@hotmail.com>
2018-03-24 14:36:36 +0100

    fixing locatie_model

--- application/models/Locatie_model.php
+++ application/models/Locatie_model.php
@@ -1,44 +1,4 @@
 <?php
-<<<<<<< HEAD
-class Locatie_model extends CI_Model
-{
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    public function get($id)
-    {
-        $this->db->where('id', $id);
-        $query = $this->db->get('locatie');
-        return $query->row();
-    }
-
-    public function getAll()
-    {
-        $query = $this->db->get('locatie');
-        return $query->result();
-    }
-
-    public function update($locatie)
-    {
-        $this->db->where('id', $locatie->id);
-        $this->db->update('locatie_model', $locatie);
-    }
-
-    public function delete($id)
-    {
-        $this->db->where('id', $id);
-        if (!$this->db->delete('locatie')) {
-            $error = $this->db->error();
-            $msg = "Locatie heeft evenement!";
-						return $msg;
-        } else {
-            $msg = "Locatie verwijderd!";
-						return $msg;
-        }
-    }
-=======
 /**
 * @class Locatie_model
 * @brief Model-klasse voor locatieen
@@ -114,11 +74,4 @@ class Locatie_model extends CI_Model {
 		$this->db->insert('locatie', $locatie);
 		return $this->db->insert_id();
 	}
->>>>>>> Comments toevoegen bij alle models try #5
-
-    public function insert($locatie)
-    {
-        $this->db->insert('locatie_model', $locatie);
-        return $this->db->insert_id();
-    }
 }
