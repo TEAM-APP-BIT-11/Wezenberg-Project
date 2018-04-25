ca16c902d2fc48acb8745fc3629568dd7257379a
dieterverboven <dieter.verboven@outlook.com>
2018-04-25 20:16:30 +0200

    gebruikers beheren

---
+++ application/controllers/trainer/Gebruiker.php
@@ -0,0 +1,158 @@
+<?php
+
+/*
+ * @class Evenement
+ * @brief Controller-klasse voor evenementen
+ * 
+ * Controller-klasse met alle methodes die gebruikt worden in vervand met evenementen
+ */
+
+class Gebruiker extends CI_Controller
+{
+
+    /*
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!$this->authex->isAangemeld()) {
+            redirect('Welcome/logIn');
+        } else {
+            $persoon = $this->authex->getPersoonInfo();
+            if ($persoon->typePersoon->typePersoon !== "trainer") {
+                redirect('');
+            }
+        }
+
+        $this->load->helper('form');
+        $this->load->helper('notation');
+    }
+
+    /*
+     * Haalt alle evenement-records op via Evenement_model en toont de lijst in de view evenementen_beheren.php
+     * 
+     * @see Evenement_mode::getAll()
+     * @see evenementen_beheren.php
+     */
+    public function beheren()
+    {
+        $data['titel'] = "Gebruikers beheren";
+        $data['eindverantwoordelijke'] = "Dieter Verboven";
+        $this->load->model('persoon_model');
+        $data['zwemmers'] = $this->persoon_model->getZwemmers();
+        $data['trainers'] = $this->persoon_model->getTrainers();
+
+        $partials = array('inhoud' => 'trainer/gebruikers_beheren', 'footer' => 'main_footer');
+        $this->template->load('main_master', $partials, $data);
+    }
+    
+    public function aanpassen($id)
+    {
+        $data['titel'] = "Gebruiker aanpassen";
+        $data['eindverantwoordelijke'] = "Dieter Verboven";
+        
+        $this->load->model('persoon_model');
+        $data['persoon'] = $this->persoon_model->get($id);
+        
+        $this->load->model('typepersoon_model');
+        $data['types'] = $this->typepersoon_model->getAll();
+
+        $partials = array('inhoud' => 'trainer/gebruiker_aanpassen', 'footer' => 'main_footer');
+        $this->template->load('main_master', $partials, $data);
+    }
+    
+    public function toevoegen()
+    {
+        $data['titel'] = "Gebruiker toevoegen";
+        $data['eindverantwoordelijke'] = "Dieter Verboven";
+        
+        $this->load->model('typepersoon_model');
+        $data['types'] = $this->typepersoon_model->getAll();
+
+        $partials = array('inhoud' => 'trainer/gebruiker_toevoegen', 'footer' => 'main_footer');
+        $this->template->load('main_master', $partials, $data);
+    }
+
+    public function pasAan()
+    {
+        $persoon = new stdClass();
+
+        $persoon->id = $this->input->post('id');
+        $persoon->typePersoonId = $this->input->post('type');
+        $persoon->voornaam = $this->input->post('voornaam');
+        $persoon->familienaam = $this->input->post('familienaam');
+        $persoon->geboortedatum = $this->input->post('geboortedatum');
+        $persoon->straat = $this->input->post('straat');
+        $persoon->nummer = $this->input->post('nummer');
+        $persoon->mailadres = $this->input->post('mailadres');
+        $persoon->gsmnummer = $this->input->post('gsmnummer');
+        $persoon->woonplaats = $this->input->post('woonplaats');
+        $persoon->postcode = $this->input->post('postcode');
+        $persoon->gebruikersnaam = $this->input->post('gebruikersnaam');
+
+        $this->load->model('persoon_model');
+
+        $this->persoon_model->update($persoon);
+        
+
+        $this->beheren();
+    }
+    
+    
+    public function voegToe()
+    {
+        $persoon = new stdClass();
+        
+        $type = $this->input->post('type');
+
+        $persoon->typePersoonId = $type;
+        $persoon->voornaam = $this->input->post('voornaam');
+        $persoon->familienaam = $this->input->post('familienaam');
+        $persoon->geboortedatum = $this->input->post('geboortedatum');
+        $persoon->straat = $this->input->post('straat');
+        $persoon->nummer = $this->input->post('nummer');
+        $persoon->mailadres = $this->input->post('mailadres');
+        $persoon->gsmnummer = $this->input->post('gsmnummer');
+        $persoon->woonplaats = $this->input->post('woonplaats');
+        $persoon->postcode = $this->input->post('postcode');
+        $persoon->gebruikersnaam = $this->input->post('gebruikersnaam');
+        if ($type == 1)
+        {
+            $persoon->wachtwoord = 'trainer123';
+        }
+        else
+        {
+            $persoon->wachtwoord = 'zwemmer123';
+        }
+        $persoon->actief = '1';
+
+        $this->load->model('persoon_model');
+
+        $this->persoon_model->insert($persoon);
+        
+
+        $this->beheren();
+    }
+    
+     public function activiteitVeranderen($id)
+    {
+        $this->load->model('persoon_model'); 
+        $persoon = $this->persoon_model->get($id);
+        
+        if($persoon->actief == 1)
+        {
+            $persoon->actief = '0';
+        } 
+        else
+        {
+            $persoon->actief = '1';
+        }
+
+        $this->persoon_model->update($persoon);
+        
+
+        $this->beheren();
+    }
+}
--- application/views/trainer/gebruiker_toevoegen.php
+++ application/views/trainer/gebruiker_toevoegen.php
@@ -1,15 +1,15 @@
 <div class="col-md-10 content">
 
-    <h1 class="">Gebruiker aanpassen</h1>
+    <h1 class="">Gebruiker toevoegen</h1>
     <hr>
    
     <?php
     foreach ($types as $type) {
         $typeOpties[$type->id] = $type->typePersoon;
     }
-    
+
     $attributes = array('name' => 'gebruikerAanpassenFormulier');
-    echo form_open('trainer/gebruiker/pasAan', $attributes);
+    echo form_open('trainer/gebruiker/voegToe', $attributes);
 
     echo form_labelpro('Type gebruiker:', 'type');
     echo form_dropdown('type', $typeOpties);
@@ -19,7 +19,6 @@
     echo form_labelpro('Voornaam', 'voornaam');
     echo form_input(array('name' => 'voornaam',
         'id' => 'voornaam',
-        'value' => $persoon->voornaam,
         'class' => 'form-control',
         'required' => 'required'));
 
@@ -28,7 +27,6 @@
     echo form_labelpro('Familienaam', 'familienaam');
     echo form_input(array('name' => 'familienaam',
         'id' => 'familienaam',
-        'value' => $persoon->familienaam,
         'class' => 'form-control',
         'required' => 'required'));
 
@@ -36,7 +34,6 @@
     echo form_labelpro('Geboortedatum', 'geboortedatum');
     echo form_input(array('name' => 'geboortedatum',
         'id' => 'geboortedatum',
-        'value' => $persoon->geboortedatum,
         'class' => 'form-control',
         'required' => 'required',
         'type' => 'date'));
@@ -45,7 +42,6 @@
     echo form_labelpro('Straat', 'straat');
     echo form_input(array('name' => 'straat',
         'id' => 'straat',
-        'value' => $persoon->straat,
         'class' => 'form-control'));
 
     echo '</br>';
@@ -53,7 +49,6 @@
     echo form_labelpro('Huisnummer', 'nummer');
     echo form_input(array('name' => 'nummer',
         'id' => 'nummer',
-        'value' => $persoon->nummer,
         'class' => 'form-control'));
 
     echo '</br>';
@@ -61,7 +56,6 @@
     echo form_labelpro('Postcode', 'postcode');
     echo form_input(array('name' => 'postcode',
         'id' => 'postcode',
-        'value' => $persoon->postcode,
         'class' => 'form-control'));
 
     echo '</br>';
@@ -69,7 +63,6 @@
     echo form_labelpro('Woonplaats', 'woonplaats');
     echo form_input(array('name' => 'woonplaats',
         'id' => 'woonplaats',
-        'value' => $persoon->woonplaats,
         'class' => 'form-control'));
 
     echo '</br>';
@@ -77,7 +70,6 @@
     echo form_labelpro('E-mailadres', 'mailadres');
     echo form_input(array('name' => 'mailadres',
         'id' => 'mailadres',
-        'value' => $persoon->mailadres,
         'class' => 'form-control',
         'required' => 'required'));
 
@@ -86,7 +78,6 @@
     echo form_labelpro('Gsmnummer', 'gsmnummer');
     echo form_input(array('name' => 'gsmnummer',
         'id' => 'gsmnummer',
-        'value' => $persoon->gsmnummer,
         'class' => 'form-control'));
 
     echo '</br>';
@@ -94,14 +85,12 @@
     echo form_labelpro('Gebruikersnaam', 'gebruikersnaam');
     echo form_input(array('name' => 'gebruikersnaam',
         'id' => 'gebruikersnaam',
-        'value' => $persoon->gebruikersnaam,
         'class' => 'form-control',
         'required' => 'required'));
 
     echo '</br>';
 
-    echo form_hidden('id', $persoon->id);
-    echo form_submit('knop', 'Opslaan', 'class="btn btn-primary"');
+    echo form_submit('knop', 'Voeg toe', 'class="btn btn-primary"');
     echo form_close();
     ?>
     <?php echo anchor('trainer/gebruiker/beheren', form_button('back', 'Annuleren', 'class="btn btn-warning"')) ;?>
