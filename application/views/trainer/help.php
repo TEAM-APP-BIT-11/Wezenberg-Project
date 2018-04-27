77427c4f66564e07575db308c5dcf27704f2ca6d
stefjs <r0655338@student.thomasmore.be>
2018-04-27 15:42:01 +0200

    Help functie klaar

--- application/views/trainer/help.php
+++ application/views/trainer/help.php
@@ -55,7 +55,6 @@ $(document).ready(function(){
     <p>Vul de naam, begindatum, einddatum, kies de locatie en extra informatie van de nieuwe wedstrijd in.</p>
     <p>Klik op de toevoegen knop om de wedstrijd toe te voegen.</p>
     <p>Klik op de annuleren knop om terug te gaan naar wedstrijd beheren en geen wedstrijd toe te voegen.</p>
-    <?php echo toonAfbeelding('help/wedstrijdtoevoegen.png', 'class="img-responsive"'); ?>
     <hr>
     <h3>Wedstrijd Aanpassen</h3>
     <h4>Op de wedstrijd aanpassen pagina kan je een bestaande wedstrijd aanpassen</h4>
@@ -67,13 +66,11 @@ $(document).ready(function(){
     <p>Klik op de aanpassen knop om de reeks aan de passen.</p>
     <p>Klik op de verwijder knop om de reeks te verwijderen.</p>
     <hr>
-    <?php echo toonAfbeelding('help/wedstrijdaanpassen.png', 'class="img-responsive"'); ?>
     <h3>Reeks toevoegen</h3>
     <h4>Op de reeks toevoegen pagina kan je een reeks toevoegen aan een bestaande wedstrijd</h4>
     <p>Vul de datum, beginuur, einduur, kies de afstand en slag van de reeks.</p>
     <p>Klik op de toevoegen knop om de reeks toe te voegen.</p>
     <p>Klik op de annuleren knop om terug te gaan naar wedstrijd beheren en reeks toe te voegen.</p>
-    <?php echo toonAfbeelding('help/reekstoevoegen.png', 'class="img-responsive"'); ?>
     <hr>
   </div>
 </div>
@@ -95,10 +92,8 @@ $(document).ready(function(){
     <h4>Op de evenementen toevoegen pagina kan je evenementen toevoegen</h4>
     <p class="bold">Enkel evenement</p>
     <p>Vul de gegevens van je evenement in om het aan te maken. Evenementnaam, datum, beginuur en locatie zijn hier verplichte velden. Je kan zwemmers aan het evenement toevoegen door in de lijst onderaan het formulier een zwemmer te selecteren en dan op het pijltje naar rechts te klikken. Alle deelnemende zwemmers krijgen het evenement in hun agenda te zien, en zullen ook een melding ontvangen dat het evenement aangemaakt is.</p>
-    <?php echo toonAfbeelding('help/evenemententoevoegen1.png', 'class="img-responsive"'); ?>
     <p class="bold">Enkel evenement</p>
     <p>Als je een herhalend evenement wilt toevoegen selecteer je bij het veld "hoeveelheed" de optie "reeks". Je krijgt dan de mogelijkheid om een einddatum en de dagen waarop het evenement zal doorgaan te kiezen. Als je dit doet zal op elke geselecteerde dag tussen de begin- en einddatum een evenement aangemaakt worden.</p>
-    <?php echo toonAfbeelding('help/evenemententoevoegen2.png', 'class="img-responsive"'); ?>
     <p>Klik op de knop opslaan om het evenement of de evenementreeks toe te voegen.</p>
     <p>Klik op de knop annuleren om terug te gaan naar evenementen beheren en geen evenement(en) toe te voegen.</p>
     <hr>
@@ -114,6 +109,63 @@ $(document).ready(function(){
   </div>
 </div>
 
+<div class="panel panel-default" id="gebruikersBeheren">
+  <div class="panel-heading">
+    <h3 class="panel-title">Gebruikers beheren</h3>
+  </div>
+  <div class="panel-body">
+    <h3>Acties</h3>
+    <h4>Op de gebruikers beheren pagina kan je de volgende acties uitvoeren:</h4>
+    <li>Toevoegen</li>
+    <li>Gegevens aanpassen</li>
+    <li>Actief of inactief maken</li>
+    <li>Verwijderen</li>
+    <?php echo toonAfbeelding('help/gebruikersbeheren.png', 'class="img-responsive"'); ?>
+    <hr>
+    <h3>Gebruiker Toevoegen</h3>
+    <h4>Op de gebruiker toevoegen pagina kan je een gebruiker toevoegen</h4>
+    <p>Vul de velden in met de nodige informatie.</p>
+    <p>Klik op de voeg toe knop om de gebruiker toe te voegen.</p>
+    <p>Klik op de annuleren knop om de gebruiker niet toe te voegen.</p>
+    <hr>
+    <h3>Gegevens Aanpassen</h3>
+    <h4>Op de gegevens aanpassen pagina kan je een gebruiker zijn gegevens aanpassen</h4>
+    <p>Klik op de actief knop als je een gebruiker actief wilt maken.</p>
+    <p>Klik op de inactief knop als je een gebruiker inactief wilt maken.</p>
+    <hr>
+    <h3>Actief Of Inactief Maken</h3>
+    <h4>Op de gebruikers beheren pagina kan je een gebruik inactief of actief maken</h4>
+    <p>Verander de velden waar je aanpassingen aan wilt maken.</p>
+    <p>Klik op de oplsaan knop om de aanpassingen op te slagen.</p>
+    <p>Klik op de annuleren knop om terug te gaan naar gebruikers beheren en geen aanpassingen op te slagen.</p>
+    <hr>
+  </div>
+</div>
+
+<div class="panel panel-default" id="startPaginaBeheren">
+  <div class="panel-heading">
+    <h3 class="panel-title">Startpagina beheren</h3>
+  </div>
+  <div class="panel-body">
+    <h3>Acties</h3>
+    <h4>Op de startpagine beheren pagina kan je de volgende acties uitvoeren:</h4>
+    <li>Nieuwsitem toevoegen en aanpassen</li>
+    <li>Kalender toevoegen en aanpassen</li>
+    <hr>
+    <h3>Nieuwsitem toevoegen en aanpassen</h3>
+    <h4>Op de nieuwsitem toevoegen en aanpassen pagina kan je een nieuwitem toevoegen en aanpassen</h4>
+    <p>Vul de velden in of verander ze met de nodige informatie.</p>
+    <p>Klik op de oplsaan knop om het nieuwsitem op te slaan.</p>
+    <p>Klik op de annuleren knop om het nieuwsitem niet op te slaan.</p>
+    <hr>
+    <h3>Kalender toevoegen en aanpassen</h3>
+    <h4>Op de kalender toevoegen en aanpassen pagina kan je een evenement toevoegen en aanpassen</h4>
+    <p>Vul de velden in of verander ze met de nodige informatie.</p>
+    <p>Klik op de oplsaan knop om het evenement op te slaan.</p>
+    <p>Klik op de annuleren knop om de evenement niet op te slaan.</p>
+    <hr>
+  </div>
+</div>
 
 
 <?php  ?>
