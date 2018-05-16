22f24544f60e8336ac2659a11e1e385ed5eb6d66
dieterverboven <dieter.verboven@outlook.com>
2018-05-16 15:04:04 +0200

    Als er nog geen resultaten zijn een melding geven

--- application/views/trainer/ajax_haalResultatenOp.php
+++ application/views/trainer/ajax_haalResultatenOp.php
@@ -12,35 +12,41 @@
             
             
 <?php
-
-    echo '<table class="table">
-            <tr class="active">
-                <td><label for="type">Ronde</label></td>
-                <td><label for="type">Ranking</label></td>
-                <td><label for="type">Zwemmer</label></td>
-                <td><label for="type">Resultaat</label></td>
-                <td><label for="type">Actie</label></td>
-            </tr>';
-    foreach($wedstrijddeelnames as $wedstrijddeelname)
+if(empty($wedstrijddeelnames))
     {
-        
-        if (isset($wedstrijddeelname->resultaatId))
-        {
-            echo "<tr><td> " . ucfirst($wedstrijddeelname->resultaat->rondetype->type) . "</td>"; 
-            echo "<td> " . $wedstrijddeelname->resultaat->ranking . "</td>";
-            echo "<td> " . ucfirst($wedstrijddeelname->persoon->voornaam) . "</td>";
-            echo "<td> " . $wedstrijddeelname->resultaat->tijd . "</td>"; 
-            echo "<td>" . anchor('trainer/wedstrijdresultaat/resultatenaanpassen/' . $wedstrijddeelname->resultaatId, 'Aanpassen') . "</td></tr>";
-        }
-        else
-        {
-            echo "<tr><td> leeg </td>"; 
-            echo "<td> 0 </td>";
-            echo "<td> " . ucfirst($wedstrijddeelname->persoon->voornaam) . "</td>";
-            echo "<td> leeg </td>"; 
-            echo "<td>" . anchor('trainer/wedstrijdresultaat/resultatenaanpassen/' . $wedstrijddeelname->resultaatId, 'Aanpassen') . "</td></tr>";
-        }
+        echo "<p>Er zijn nog geen resultaten</p>";
     }
+ else {
+    echo '<table class="table">
+             <tr class="active">
+                 <td><label for="type">Ronde</label></td>
+                 <td><label for="type">Ranking</label></td>
+                 <td><label for="type">Zwemmer</label></td>
+                 <td><label for="type">Resultaat</label></td>
+                 <td><label for="type">Actie</label></td>
+             </tr>';
+     foreach($wedstrijddeelnames as $wedstrijddeelname)
+     {
+
+         if (isset($wedstrijddeelname->resultaatId))
+         {
+             echo "<tr><td> " . ucfirst($wedstrijddeelname->resultaat->rondetype->type) . "</td>"; 
+             echo "<td> " . $wedstrijddeelname->resultaat->ranking . "</td>";
+             echo "<td> " . ucfirst($wedstrijddeelname->persoon->voornaam) . "</td>";
+             echo "<td> " . $wedstrijddeelname->resultaat->tijd . "</td>"; 
+             echo "<td>" . anchor('trainer/wedstrijdresultaat/resultatenaanpassen/' . $wedstrijddeelname->resultaatId, 'Aanpassen') . "</td></tr>";
+         }
+         else
+         {
+             echo "<tr><td> leeg </td>"; 
+             echo "<td> 0 </td>";
+             echo "<td> " . ucfirst($wedstrijddeelname->persoon->voornaam) . "</td>";
+             echo "<td> leeg </td>"; 
+             echo "<td>" . anchor('trainer/wedstrijdresultaat/resultatenaanpassen/' . $wedstrijddeelname->resultaatId, 'Aanpassen') . "</td></tr>";
+         }
+     }
+ }
+    
     
 ?>
 </table>
